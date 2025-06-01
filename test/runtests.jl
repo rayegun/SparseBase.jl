955c6b7364533addf2106cce583fb42e10fe35f5
Raye Kimmerer <raye@rayegun.com>
2025-06-01 12:37:57 -0400

    fix weird test inclusion

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,3 @@
 using Test
 using SparseBase
 using LinearAlgebra
-
-@testset "SparseBase.jl Tests" begin
-    
-    @testset "Basic Functionality" begin
-        @test SparseBase.novalue isa SparseBase.NoValue
-        @test SparseBase.ColMajor() isa SparseBase.StorageOrder
-        @test SparseBase.RowMajor() isa SparseBase.StorageOrder
-        @test SparseBase.RuntimeOrder() isa SparseBase.StorageOrder
